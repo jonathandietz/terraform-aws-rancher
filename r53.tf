9672ebe97a233179c4952f1725172b29f40cdd17
onejati <omidnejati.it@gmail.com>
2017-05-10 10:40:41 -0700

    Update r53.tf

--- r53.tf
+++ r53.tf
@@ -3,7 +3,7 @@
 #------------------------------------------#
 resource "aws_route53_record" "default" {
    zone_id = "${var.zone_id}"
-   name = "rancher.aerpge.com"
+   name = "rancher.com"
    type = "A"
    ttl = "300"
    records = ["${aws_instance.Rancher.0.private_ip}"]
